aff19e4f2b618266cf30b1a6b974ae93b180a0d3
DevSteg <j.l.stephens91@gmail.com>
2022-01-18 22:21:23 +0000

    Add logic and small style to the game
    - Add: logic to cycle through X and O on each turn
    - Add: logic for when a game is won
    - Style: boarder radius on the squares div

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,29 @@
 import React , {useState} from "react";
 
 export const Board = () => {
-  const [board, setBoard ] = useState(["x", "o", "", "", "", "", "", "", ""])
+  
   const [prevTurn, setPrevTurn] = useState('X')
+  const changeTurn = () => {
+    setPrevTurn(prev => prev === 'X' ? 'O':'X')
+  }
   
-  
+  const [board, setBoard ] = useState(["", "", "", "", "", "", "", "", ""])
   const handleClick = (index) => {
     let newBoard = [...board]
     newBoard[index] = prevTurn
     setBoard(newBoard)
+    let win_pattern = [
+      [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+    
+    for(let i in win_pattern) {
+      const [a, b, c] = win_pattern[i];
+      if(board[a] && board[a] === board[b] && board[a] === board[c]) {
+        alert("Winner")
+      }
+      return null
+    }
+    changeTurn()
   }
 
   return (
