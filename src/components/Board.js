99ecf2d8eecde5087a10ac7e42a683c445655078
DevSteg <j.l.stephens91@gmail.com>
2022-01-20 22:03:15 +0000

    Refactor Board component to use hooks

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,20 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 
 export const Board = () => {
-  
-  const [prevTurn, setPrevTurn] = useState('X')
-  const changeTurn = () => {
-    setPrevTurn(prev => prev === 'X' ? 'O':'X')
-  }
-  
+
   const [board, setBoard ] = useState(["", "", "", "", "", "", "", "", ""])
   const handleClick = (index) => {
     let newBoard = [...board]
     newBoard[index] = prevTurn
     setBoard(newBoard)
+  }
+
+  const [prevTurn, setPrevTurn] = useState('X')
+  useEffect(() => {
+    setPrevTurn(prev => prev === 'X' ? 'O':'X')
+  }, [board])
+  
+  useEffect(() => {
     let win_pattern = [
       [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
     ]
@@ -19,12 +22,10 @@ export const Board = () => {
     for(let i in win_pattern) {
       const [a, b, c] = win_pattern[i];
       if(board[a] && board[a] === board[b] && board[a] === board[c]) {
-        alert("Winner")
+        alert(`The winner is ${board[a]}`)
       }
-      return null
     }
-    changeTurn()
-  }
+  }, [board])
 
   return (
     <div className="Board">
